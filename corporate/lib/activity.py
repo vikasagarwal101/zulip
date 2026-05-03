28fe3b4221e80c6a3a9f49250bd1e783f1db3272
Vikas Agarwal <vikasagarwal101@users.noreply.github.com>
2026-05-04 00:24:48 +0530

    fix: resolve 9 ruff-c408 findings

--- corporate/lib/activity.py
+++ corporate/lib/activity.py
@@ -134,7 +134,7 @@ def realm_activity_link(realm_str: str) -> Markup:
 def realm_stats_link(realm_str: str) -> Markup:
     from analytics.views.stats import stats_for_realm
 
-    url = reverse(stats_for_realm, kwargs=dict(realm_str=realm_str))
+    url = reverse(stats_for_realm, kwargs={"realm_str": realm_str})
     return Markup('<a href="{url}"><i class="fa fa-pie-chart"></i></a>').format(url=url)
 
 
@@ -157,7 +157,7 @@ def realm_url_link(realm_str: str) -> Markup:
 def remote_installation_stats_link(server_id: int) -> Markup:
     from analytics.views.stats import stats_for_remote_installation
 
-    url = reverse(stats_for_remote_installation, kwargs=dict(remote_server_id=server_id))
+    url = reverse(stats_for_remote_installation, kwargs={"remote_server_id": server_id})
     return Markup('<a href="{url}"><i class="fa fa-pie-chart"></i></a>').format(url=url)
 
 
